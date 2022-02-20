bf9238c17879d28697ce4c07c1d04a722a28051b
RodrigoFurlaneto <rodrigo.ffurlaneto@gmail.com>
2022-02-20 14:41:56 -0300

    Exercicio Multiplos

--- ExercicioMultiplos/src/Main.java
+++ ExercicioMultiplos/src/Main.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int A, B, troca;
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		
+		if (A < B) {
+			troca = A;
+			A = B;
+			B = troca;
+		}
+		
+		if (A % B == 0) {
+			System.out.println("Sao multiplos");
+		}
+		else {
+			System.out.println("Nao sao multiplos");
+		}
+		
+		sc.close();
 	}
 
 }

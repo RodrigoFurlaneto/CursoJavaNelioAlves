001b4b18531a842f166df38736dccd26cc901483
RodrigoFurlaneto <rodrigo.ffurlaneto@gmail.com>
2022-03-02 21:17:14 -0300

    New exercise

---
+++ ExercicioCombustivel/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		int alcool = 0;
+		int gasolina = 0;
+		int diesel = 0;
+		
+		while (x != 4) {
+			switch (x) {
+			case 1:
+				alcool++;
+				break;
+			case 2:
+				gasolina++;
+				break;
+			case 3:
+				diesel++;
+				break;
+			}
+			x = sc.nextInt();
+		}
+		
+		System.out.println("MUITO OBRIGADO");
+		System.out.println("Alcool: " + alcool);
+		System.out.println("Gasolina: " + gasolina);
+		System.out.println("Diesel: " + diesel);
+		
+		sc.close();
+	}
+}
